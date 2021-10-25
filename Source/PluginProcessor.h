66318d9f140c4e04038f2659a331950fc4d847ad
Szilard <szilard.fer@gmail.com>
2021-10-25 14:43:16 +0200

    work

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -40,6 +40,11 @@ enum ChainPositions
   HighCut
 };
 
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+
 
 
 class SimpleEQAudioProcessor  : public juce::AudioProcessor
@@ -95,9 +100,6 @@ private:
   MonoChain leftChain, rightChain;
 
   void updatePeakFilter(const ChainSettings& chainSetting);
-
-  using Coefficients = Filter::CoefficientsPtr;
-  static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
 
   template<int Index, typename ChainType, typename CoefficientType>
   void update(ChainType& chain, const CoefficientType& coefficients)
